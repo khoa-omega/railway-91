10256a2ebad606b3d855cde595a2aaccb205d2b0
khoa-omega <nvkhoa05@gmail.com>
2024-12-27 21:47:45 +0700

    [Database] Update lesson-03

--- Database/assignment-03.sql
+++ Database/assignment-03.sql
@@ -276,7 +276,74 @@ SELECT department_id
 FROM department
 WHERE department_name = "Sale";
 
--- Question 6: Lấy ra tên group đã tham gia
--- trước ngày 20/12/2019
+-- Question 4: Lấy ra account có full name dài nhất
+SELECT *
+FROM account
+ORDER BY CHAR_LENGTH(full_name) DESC
+LIMIT 1;
 
+-- Question 5: Lấy ra account có full name dài nhất
+-- và thuộc phòng ban có id = 3
+SELECT *
+FROM account
+WHERE department_id = 3
+ORDER BY CHAR_LENGTH(full_name) DESC
+LIMIT 1;
 
+-- Question 6: Lấy ra tên group tạo trước ngày 20/12/2019
+SELECT *
+FROM `group`
+WHERE create_date < "2019-12-20";
+
+-- Question 7: Lấy ra id của question có >= 4 câu trả lời
+SELECT question_id
+FROM answer
+GROUP BY question_id
+HAVING COUNT(answer_id) >= 4;
+
+-- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút
+-- và được tạo trước ngày 20/12/2019
+SELECT code
+FROM exam
+WHERE duration >= 60 AND create_date < "2019-12-20";
+
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT *
+FROM `group`
+ORDER BY create_date DESC
+LIMIT 5;
+
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT COUNT(account_id)
+FROM account
+WHERE department_id = 2;
+
+-- Question 11: Lấy ra nhân viên có tên
+-- bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
+SELECT *
+FROM account
+WHERE full_name LIKE "D%o";
+
+-- Question 12: Xóa tất cả các exam
+-- được tạo trước ngày 20/12/2019
+DELETE FROM exam
+WHERE create_date < "2019-12-20";
+
+-- Question 13: Xóa tất cả các question
+-- có nội dung bắt đầu bằng từ "câu hỏi"
+DELETE FROM question
+WHERE content LIKE "câu hỏi%";
+
+-- Question 14: Update thông tin của account
+-- có id = 5 thành tên "Nguyễn Bá Lộc" và
+-- email thành loc.nguyenba@vti.com.vn
+UPDATE account
+SET full_name = "Nguyễn Bá Lộc",
+    email = "loc.nguyenba@vti.com.vn"
+WHERE account_id = 5;
+
+-- Question 15: Update account có id = 5
+-- sẽ thuộc group có id = 4
+UPDATE group_account
+SET group_id = 4
+WHERE account_id = 5;
--- Database/lesson-03.sql
+++ Database/lesson-03.sql
@@ -23,14 +23,90 @@ FROM exam;
 SELECT *
 FROM account
 WHERE account_id > 5;
+-- Toán tử: AND, OR
+-- VD: Lấy ra account có department_id = 1 và position_id = 1
+SELECT *
+FROM account
+WHERE department_id = 1 AND position_id = 1;
+-- VD: Lấy ra account có department_id = 1 hoặc position_id = 1
+SELECT *
+FROM account
+WHERE department_id = 1 OR position_id = 1;
+-- Toán tử BETWEEN ... AND ...
+-- VD: Lấy ra department có 5 <= id <= 8
+SELECT *
+FROM department
+WHERE department_id BETWEEN 5 AND 8;
+-- Toán tử IN / NOT IN
+-- VD: Lấy ra department có id = any (2, 4, 6, 8)
+SELECT *
+FROM department
+WHERE department_id IN (2, 4, 6, 8);
+-- Toán tử LIKE / NOT LIKE
+-- _: Đại diện cho 1 kí tự bất kì
+-- %: Đại diện cho 0 hoặc nhiều kí tự bất kì
+-- VD: Lấy ra phòng có tên chứa kí tự "n"
+SELECT *
+FROM department
+WHERE department_name LIKE "%n%";
+-- Toán tử IS NULL / IS NOT NULL
+-- VD: Lấy ra department có tên
+SELECT *
+FROM department
+WHERE department_name IS NOT NULL;
 
+-- Mệnh đề ORDER BY
+-- ASC: Tăng dần (Mặc định)
+-- DESC: Giảm dần
+-- VD: Lấy ra exam sắp xếp
+-- duration giảm dần, create_date giảm dần
+SELECT *
+FROM exam
+ORDER BY duration DESC, create_date DESC;
 
+-- Mệnh đề LIMIT
+-- VD: Lấy ra 2 đề thi
+SELECT *
+FROM exam
+LIMIT 2;
 
+-- Các hàm tổng hợp
+-- COUNT, SUM, MIN, MAX, AVG
+-- COUNT(*): Đếm số dòng
+-- COUNT(duration): Đếm số dòng có duration khác NULL
+-- COUNT(DISTINCT duration): Đếm số dòng có duration khác nhau
+-- Chú ý: Chỉ áp dụng cho các giá trị khác NULL
+-- VD:
+SELECT
+    COUNT(*),
+    COUNT(duration),
+    COUNT(DISTINCT duration),
+    SUM(duration),
+    MIN(duration),
+    MAX(duration),
+    AVG(duration)
+FROM exam;
 
+-- Mệnh đề GROUP BY
+-- VD: Thống kê số nhân viên trong từng phòng ban
+SELECT department_id, COUNT(account_id) AS account_count
+FROM account
+GROUP BY department_id;
 
+-- Mệnh đề HAVING
+-- VD: Lấy ra phòng ban có > 2 nhân viên
+SELECT department_id, COUNT(account_id) AS account_count
+FROM account
+GROUP BY department_id
+HAVING COUNT(account_id) > 2;
 
+-- Cập nhật
+-- VD: Đổi tên phòng ban số 1 thành "Phòng chờ"
+UPDATE department
+SET department_name = "Phòng chờ"
+WHERE department_id = 1;
 
-
-
-
-
+-- Xóa dữ liệu
+-- VD: Xóa phòng ban số 1
+DELETE FROM department
+WHERE department_id = 1;
